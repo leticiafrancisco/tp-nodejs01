0bc5f1792a05ef3b7e79b3a039e938f8f6e29927
[leticiafrancisco] <leticia.santos1105@etec.sp.gov.br>
2020-09-24 19:14:42 -0300

    Instação do Express

---
+++ index.js
@@ -0,0 +1,10 @@
+/* IMPORTAÇÃO DO PACOTE EXPRESS PARA A INDEX */
+// DECLARAR UM CONTANTE
+const express = require('express')
+ 
+const app = express()
+ 
+app.listen(app.get('3000'), ()=>{
+ 
+ console.log('SERVIDOR RODANDO')
+})
