fcbffe90d28a6ede102092d105cb230d15165475
[leticiafrancisco] <leticia.santos1105@etec.sp.gov.br>
2020-10-15 20:36:36 -0300

    Atualização inserido GET

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 /* IMPORTAÇÃO DO PACOTE EXPRESS PARA A INDEX */
-// DECLARAR UM CONTANTE
+// DECLARAR UM CONSTANTE
 const express = require('express')
  
+/*INSTANCIA DE PACOTE*/
 const app = express()
+//app.use(express.static("public"));
  
-app.listen(app.get('3000'), ()=>{
+app.get("/", function(request, response){
+ 
+ console.log('DEU CERTO!!!!')
+ 
+});
+ 
+/*-------------------------------------------------------------------------------------------*/
+/*ÁREA DE TESTE*/
+ 
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 2')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 3')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 4')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 5')
+ 
+/*-------------------------------------------------------------------------------------------*/
+ 
+/* CRIAÇÃO DO SERVIDO WEB DA APLICAÇÃO */
+app.listen('3000', ()=>{
  
  console.log('SERVIDOR RODANDO')
+ 
 })
